1ed20ca346e8b3d5e9cea71f4b1bd84f470de7be
Roxana Chinchilla <roxy.0507@gmail.com>
2021-01-29 15:58:21 -0600

    Arrays and methods

---
+++ exercises/0_sum.rb
@@ -0,0 +1,19 @@
+=begin
+suma_n.rb: El usuario ingresa un número, se muestra la suma de todos los números de 1 hasta
+ese número.
+Uso:
+suma_n.rb 100
+5050
+=end
+
+number = ARGV[0].to_i
+
+i= 0
+sum = 0 
+
+while (i > number)
+    i += 1
+    sum += i 
+end
+puts sum
+
